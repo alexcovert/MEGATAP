39516f59c26dcdad654e6f7c07f7421a63cd7195
AndyLongUCSC <andlong@ucsc.edu>
2019-03-13 16:24:13 -0700

    Jump off traps

--- Assets/Scripts/Players/PlayerGrounded.cs
+++ Assets/Scripts/Players/PlayerGrounded.cs
@@ -18,7 +18,7 @@ public class PlayerGrounded : MonoBehaviour {
 
     private void OnTriggerStay(Collider collision)
     {
-        if (collision.gameObject.tag == "Platform")
+        if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Trap")
         {
             grounded = true;
         }
@@ -26,7 +26,7 @@ public class PlayerGrounded : MonoBehaviour {
 
     private void OnTriggerExit(Collider collision)
     {
-        if (collision.gameObject.tag == "Platform")
+        if (collision.gameObject.tag == "Platform" || collision.gameObject.tag == "Trap")
         {
             grounded = false;
         }
