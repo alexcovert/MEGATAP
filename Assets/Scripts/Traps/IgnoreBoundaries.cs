a58fc790d0c4787e03f49b16268552cde0fe5b38
AndyLongUCSC <andlong@ucsc.edu>
2019-05-15 14:35:17 -0700

    Logs ignore boundary

---
+++ Assets/Scripts/Traps/IgnoreBoundaries.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreBoundaries : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Physics.IgnoreLayerCollision(2, 19);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+    }
+}
