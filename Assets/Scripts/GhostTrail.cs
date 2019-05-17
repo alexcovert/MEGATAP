bc48493ebb63ce648f8fda63eaedd302821a0853
Peter Tieu <ptieu@ucsc.edu>
2019-05-17 11:56:35 -0700

    Started the trail speedup effect
    - Added 2D sprite asset for the trail
    - Added basic script for making the trail appear behind Speccy

---
+++ Assets/Scripts/GhostTrail.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostTrail : MonoBehaviour {
+
+	private float timeBetween;
+	public float startTime;
+
+	public GameObject ghost;
+//	private Player player;
+
+	void Start () {
+		//player = GetComponent<Player>();
+	}
+
+	void Update () {
+		if(timeBetween<0)
+		{
+			GameObject instance = (GameObject)Instantiate(ghost, transform.position, Quaternion.identity);
+			Destroy(instance, 3f);
+			timeBetween = startTime;
+		}else
+		{
+			timeBetween -= Time.deltaTime;
+		}
+	}
+}
