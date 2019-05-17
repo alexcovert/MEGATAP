73eaac739349fab967065fbb675e781db0000ba1
Alex Covert <acovert@ucsc.edu>
2019-05-17 14:47:03 -0700

    Grey out character select with mouse & keyboard

--- Assets/Scripts/UI/Menus/GameOverMenu.cs
+++ Assets/Scripts/UI/Menus/GameOverMenu.cs
@@ -3,9 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameOverMenu : MonoBehaviour {
     [SerializeField] private EventSystem es;
+    [SerializeField] private GameObject charSelectButton;
+
+    private CheckControllers cc;
+    private TextMeshProUGUI charSelectText;
+
+    private void Start()
+    {
+        cc = GameObject.Find("InputManager").GetComponent<CheckControllers>();
+        charSelectText = charSelectButton.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if(cc != null && charSelectText != null)
+        {
+            if (!(cc.GetControllerOneState() || cc.GetControllerTwoState()))
+            {
+                charSelectButton.GetComponent<Button>().interactable = false;
+                charSelectText.color = new Color(charSelectText.color.r, charSelectText.color.g, charSelectText.color.b, 0.5f);
+            }
+            else
+            {
+                charSelectButton.GetComponent<Button>().interactable = true;
+                charSelectText.color = new Color(charSelectText.color.r, charSelectText.color.g, charSelectText.color.b, 1);
+            }
+        }
+    }
 
     public void onClickRetry()
     {
