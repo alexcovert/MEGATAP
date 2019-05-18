531b169f9c934ca0662c9f64f4b02399c5328b26
Alex Covert <acovert@ucsc.edu>
2019-05-17 19:54:17 -0700

    Fix lights and controller

--- Assets/Scripts/Input&ChacterSelection/CharacterSelect.cs
+++ Assets/Scripts/Input&ChacterSelection/CharacterSelect.cs
@@ -51,6 +51,7 @@ public class CharacterSelect : MonoBehaviour {
     private bool p2LockIn = false;
     [SerializeField] private Light ollieLight;
     [SerializeField] private Light speccyLight;
+    [SerializeField] private GameObject objToFollow;
 
     float quarterDist;
 
@@ -73,8 +74,7 @@ public class CharacterSelect : MonoBehaviour {
         Vector2 playerOnePos = playerOneSelector.transform.position;
         Vector2 playerTwoPos = playerTwoSelector.transform.position;
         quarterDist = Screen.height / 4;
-        //ChangeColors();
-
+        SpotlightFollow();
 
         //Movement
         //If only one controller is plugged in
@@ -170,20 +170,20 @@ public class CharacterSelect : MonoBehaviour {
     
     private void LockIn()
     {
-        //If one controller / one keyboard
-        if(!checkControllers.GetControllerOneState())
+        //If one controller / one keyboard----------------------------------------------------------------------------------------
+        if (!checkControllers.GetControllerOneState())
         {
             //Player one lock in
             if (selectorOneState != 0 && Input.GetButtonDown("Enter") && !p1LockIn)
             {
                 //Don't allow them to lock in in same character
-                if(!p2LockIn || selectorOneState == -selectorTwoState)
+                if (!p2LockIn || selectorOneState == -selectorTwoState)
                 {
                     p1LockIn = true;
                     audioSource.PlayOneShot(lockInSFX);
 
                     //Spotlight
-                    if(selectorOneState == 1)
+                    if (selectorOneState == 1)
                     {
                         ollieLight.enabled = true;
                     }
@@ -209,10 +209,45 @@ public class CharacterSelect : MonoBehaviour {
                     speccyLight.enabled = false;
                 }
             }
+
+            //Player two lock in
+            if (selectorTwoState != 0 && Input.GetButtonDown("Submit_Menu_Joy_1") && !p2LockIn)
+            {
+                if (!p1LockIn || selectorOneState == -selectorTwoState)
+                {
+                    p2LockIn = true;
+                    audioSource.PlayOneShot(lockInSFX);
+
+                    //Spotlight
+                    if (selectorTwoState == 1)
+                    {
+                        ollieLight.enabled = true;
+                    }
+                    else
+                    {
+                        speccyLight.enabled = true;
+                    }
+                }
+            }
+            //Player two cancel
+            if (selectorTwoState != 0 && p2LockIn && Input.GetButtonDown("Cancel_Joy_1"))
+            {
+                p2LockIn = false;
+                audioSource.PlayOneShot(unlockSFX);
+
+                if (selectorTwoState == 1)
+                {
+                    ollieLight.enabled = false;
+                }
+                else
+                {
+                    speccyLight.enabled = false;
+                }
+            }
         }
         else
         {
-            //If both controllers plugged in
+            //If both controllers plugged in----------------------------------------------------------------------------------------
 
             //Player one lock in
             if (selectorOneState != 0 && Input.GetButtonDown("Submit_Menu_Joy_1") && !p1LockIn)
@@ -249,40 +284,40 @@ public class CharacterSelect : MonoBehaviour {
                     speccyLight.enabled = false;
                 }
             }
-        }
 
-        //Player two lock in
-        if (selectorTwoState != 0 && Input.GetButtonDown("Submit_Menu_Joy_2") && !p2LockIn)
-        {
-            if(!p1LockIn || selectorOneState == -selectorTwoState)
+            //Player two lock in
+            if (selectorTwoState != 0 && Input.GetButtonDown("Submit_Menu_Joy_2") && !p2LockIn)
             {
-                p2LockIn = true;
-                audioSource.PlayOneShot(lockInSFX);
+                if (!p1LockIn || selectorOneState == -selectorTwoState)
+                {
+                    p2LockIn = true;
+                    audioSource.PlayOneShot(lockInSFX);
 
-                //Spotlight
+                    //Spotlight
+                    if (selectorTwoState == 1)
+                    {
+                        ollieLight.enabled = true;
+                    }
+                    else
+                    {
+                        speccyLight.enabled = true;
+                    }
+                }
+            }
+            //Player two cancel
+            if (selectorTwoState != 0 && p2LockIn && Input.GetButtonDown("Cancel_Joy_2"))
+            {
+                p2LockIn = false;
+                audioSource.PlayOneShot(unlockSFX);
+
                 if (selectorTwoState == 1)
                 {
-                    ollieLight.enabled = true;
+                    ollieLight.enabled = false;
                 }
                 else
                 {
-                    speccyLight.enabled = true;
+                    speccyLight.enabled = false;
                 }
-            }
-        }
-        //Player two cancel
-        if (selectorTwoState != 0 && p2LockIn && Input.GetButtonDown("Cancel_Joy_2"))
-        {
-            p2LockIn = false;
-            audioSource.PlayOneShot(unlockSFX);
-
-            if (selectorTwoState == 1)
-            {
-                ollieLight.enabled = false;
-            }
-            else
-            {
-                speccyLight.enabled = false;
             }
         }
 
@@ -294,7 +329,7 @@ public class CharacterSelect : MonoBehaviour {
         //Check characters selected are opposite to allow scene start
         if (selectorOneState == -selectorTwoState && selectorOneState != 0 && p1LockIn && p2LockIn)
         {
-            startText.text = "Press Start to Start!";
+            startText.text = "Press Start!";
             if (inputManager.GetButtonDown(InputCommand.Start) && checkControllers.GetControllerOneState())
             {
                 if (selectorOneState == -1)
@@ -319,20 +354,17 @@ public class CharacterSelect : MonoBehaviour {
             {
                 if (selectorOneState == -1)
                 {
-                    Debug.Log("P1 is top");
                     inputManager.P1IsTop = true;
                     checkControllers.topPlayersController = true;
                     Cursor.lockState = CursorLockMode.Locked;
                 }
                 else
                 {
-                    Debug.Log("P1 is bottom");
                     inputManager.P1IsTop = false;
                 }
 
                 if (inputManager.TutorialSelected)
                 {
-                    //Initiate.Fade("Tutorial", Color.black, 2);
                     StartCoroutine(loader.LoadScene("Tutorial"));
                 }
                 else
@@ -343,65 +375,14 @@ public class CharacterSelect : MonoBehaviour {
         }
         else
         {
-            startText.text = "";
+            startText.text = "Choose your side!";
         }
     }
     
-
-    //private void ChangeColors()
-    //{
-    //    //Controller images
-    //    switch(selectorOneState)
-    //    {
-    //        case -1:
-    //            playerOneSelector.sprite = controllerRed;
-    //            break;
-    //        case 0:
-    //            playerOneSelector.sprite = controllerGrey;
-    //            break;
-    //        case 1:
-    //            playerOneSelector.sprite = controllerBlue;
-    //            break;
-    //    }
-
-    //    switch (selectorTwoState)
-    //    {
-    //        case -1:
-    //            playerTwoSelector.sprite = controllerRed;
-    //            break;
-    //        case 0:
-    //            playerTwoSelector.sprite = controllerGrey;
-    //            break;
-    //        case 1:
-    //            playerTwoSelector.sprite = controllerBlue;
-    //            break;
-    //    }
-
-    //    //Background & characters
-    //    if(selectorOneState == 1 || selectorTwoState == 1)
-    //    {
-    //        topBackgroundObj.sprite = topBackgroundColored;
-    //        topCharObj.sprite = topCharColored;
-    //    }
-    //    else
-    //    {
-    //        topBackgroundObj.sprite = topBackgroundGrey;
-    //        topCharObj.sprite = topCharGrey;
-    //    }
-
-    //    if(selectorOneState == -1 || selectorTwoState == -1)
-    //    {
-    //        botBackgroundObj.sprite = bottomBackgroundColored;
-    //        bottomCharObj.sprite = bottomCharColored;
-    //    }
-    //    else
-    //    {
-    //        botBackgroundObj.sprite = bottomBackgroundGrey;
-    //        bottomCharObj.sprite = bottomCharGrey;
-    //    }
-
-
-    //}
+    private void SpotlightFollow()
+    {
+        speccyLight.transform.position = new Vector3(objToFollow.transform.position.x, speccyLight.transform.position.y, objToFollow.transform.position.z);
+    }
 
     private IEnumerator StickDelay()
     {
