04778a7ae7a743967479fcb89b2311114e74e0cf
AndyLongUCSC <andlong@ucsc.edu>
2019-05-31 14:51:12 -0700

    Adjusted mushroom placement

--- Assets/Scripts/Editor/PlaceMushrooms.cs
+++ Assets/Scripts/Editor/PlaceMushrooms.cs
@@ -15,8 +15,6 @@ public class PlaceMushrooms : MonoBehaviour {
 
     private static int NumMushroomsTotal = 0;
 
-    private static int floor = 1;
-
     [MenuItem("GameObject/Generate Mushrooms", false, 17)]
 
     // Use this for initialization
@@ -53,32 +51,23 @@ public class PlaceMushrooms : MonoBehaviour {
                                     {
                                         if (random == 1)
                                         {
-                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.9f, 0.9f), r.position.y + 1.1f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.identity);
+                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.8f, 0.8f), r.position.y + 1.3f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.identity);
                                             mushroom.transform.Rotate(0, Random.Range(0, 360), 0);
-
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFloor(floor);
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFace(i);
 
                                             mushroom.transform.parent = mushrooms.transform;
 
                                         }
                                         else if (random == 2)
                                         {
-                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.9f, 0.9f), r.position.y + 1.4f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.identity);
+                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.8f, 0.8f), r.position.y + 1.8f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.identity);
                                             mushroom.transform.Rotate(0, Random.Range(0, 360), 0);
-
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFloor(floor);
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFace(i);
 
                                             mushroom.transform.parent = mushrooms.transform;
                                         }
                                         else if (random == 3)
                                         {
-                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.9f, 0.9f), r.position.y + 1.15f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.identity);
+                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.8f, 0.8f), r.position.y + 1.35f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.identity);
                                             mushroom.transform.Rotate(0, Random.Range(0, 360), 0);
-
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFloor(floor);
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFace(i);
 
                                             mushroom.transform.parent = mushrooms.transform;
                                         }
@@ -89,51 +78,36 @@ public class PlaceMushrooms : MonoBehaviour {
                                     {
                                         if (random == 0)
                                         {
-                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.9f, 0.9f), r.position.y + 1.15f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
+                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.8f, 0.8f), r.position.y + 1.35f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
                                             mushroom.transform.Rotate(0, 0, Random.Range(0, 360));
-
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFloor(floor);
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFace(i);
 
                                             mushroom.transform.parent = mushrooms.transform;
                                         }
                                         else if (random == 4)
                                         {
-                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.9f, 0.9f), r.position.y + 0.95f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
+                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.8f, 0.8f), r.position.y + 0.95f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
                                             mushroom.transform.Rotate(0, 0, Random.Range(0, 360));
-
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFloor(floor);
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFace(i);
 
                                             mushroom.transform.parent = mushrooms.transform;
                                         }
                                         else if (random == 5)
                                         {
-                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.9f, 0.9f), r.position.y + 1.2f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
+                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.8f, 0.8f), r.position.y + 1.4f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
                                             mushroom.transform.Rotate(0, 0, Random.Range(0, 360));
-
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFloor(floor);
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFace(i);
 
                                             mushroom.transform.parent = mushrooms.transform;
                                         }
                                         else if (random == 6)
                                         {
-                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.9f, 0.9f), r.position.y + 1f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
+                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.8f, 0.8f), r.position.y + 1f, r.position.z + Random.Range(-0.9f, 0.9f)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
                                             mushroom.transform.Rotate(0, 0, Random.Range(0, 360));
-
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFloor(floor);
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFace(i);
 
                                             mushroom.transform.parent = mushrooms.transform;
                                         }
                                         else if (random == 7)
                                         {
-                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-1, 1), r.position.y + 1.105f, r.position.z + Random.Range(-1, 1)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
+                                            GameObject mushroom = Instantiate(Mushrooms[random], new Vector3(r.position.x + Random.Range(-0.8f, 0.8f), r.position.y + 1.305f, r.position.z + Random.Range(-1, 1)), Quaternion.Euler(new Vector3(-90f, 0, 0)));
                                             mushroom.transform.Rotate(0, 0, Random.Range(0, 360));
-
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFloor(floor);
-                                            mushroom.GetComponent<CheckMushroomRenderer>().SetFace(i);
 
                                             mushroom.transform.parent = mushrooms.transform;
                                         }
@@ -146,17 +120,12 @@ public class PlaceMushrooms : MonoBehaviour {
                         }
                     }
                 }
-                if (i % 4 == 0)
-                {
-                    floor++;
-                }
             }
         }
         faces.Clear();
         Mushrooms.Clear();
         Debug.Log("Total Number of Mushrooms Generated: " + NumMushroomsTotal);
         NumMushroomsTotal = 0;
-        floor = 1;
     }
     public static List<GameObject> GetAtPath(List<GameObject> list)
     {
