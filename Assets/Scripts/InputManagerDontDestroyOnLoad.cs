532f86de95c0b1497e62233db3c73da0e5bab3e2
Alex Covert <acovert@ucsc.edu>
2019-10-11 14:50:05 -0700

    Get AppID working

---
+++ Assets/Scripts/InputManagerDontDestroyOnLoad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class InputManagerDontDestroyOnLoad : MonoBehaviour
+{
+    private static InputManagerDontDestroyOnLoad instanceRef;
+
+    void Awake()
+    {
+        if (instanceRef == null)
+        {
+            instanceRef = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+}
